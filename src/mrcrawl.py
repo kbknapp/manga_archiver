1ddf311e0707b9db740ff8f0d4bbe9b4839f5c58
Kevin K <kbknapp@gmail.com>
2016-03-24 11:00:19 -0400

    imp: uses clapp-py for argument parsing

--- src/mrcrawl.py
+++ src/mrcrawl.py
@@ -5,6 +5,7 @@ from lxml import html
 import requests
 import hashlib
 from functools import partial
+import clapp
 
 # TODO:
 #  * Add files to dirs
@@ -16,17 +17,47 @@ def md5sum(filename):
             d.update(buf)
     return d.hexdigest()
 
+def parse_cli():
+    app = clapp.App()
+    app.name = 'MR Crawl'
+    app.author = 'Kevin K. <kbknapp@gmail.com>'
+    app.version = '0.1'
+    app.about = 'Command Line utility to download manga into CBR files'
+    
+    app.new_arg('manga', \
+            index=1, \
+            required=True, \
+            args_taken=1, \
+            help='The name of the manga, lowercase, hyphens for spaces')
+    app.new_arg('page', \
+            short='-p', \
+            long='--page',\
+            args_taken=1, \
+            help='Start at page number')
+    app.new_arg('chapter', \
+            short='-c', \
+            long='--chapter',\
+            args_taken=1, \
+            help='Start at chapter number')
+
+    return app.start()
+
 def main():
+    cxt = parse_cli()
     # prefix for CBR and JPG files
-    manga = ''
+    manga = cxt['manga']
     # without trailing chapter, page, or '/'
-    manga_base_url =  ''
+    manga_base_url =  'http://www.mangareader.net/{}'.format(manga)
     file_name = '{}_{:0>3}_{:0>3}.jpg'
     cbr = '{}_ch{:0>3}.cbr'
     manga_url = '{}/{}/{}'
 
     chapter = 0
+    if cxt['chapter']:
+        chapter = int(cxt['chapter']) - 1
     page = 0
+    if cxt['page']:
+        chapter = int(cxt['page']) - 1
     curr = ''
     prev = ''
     dups = 0
