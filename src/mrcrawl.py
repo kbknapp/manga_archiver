bcde47f027366d76995d8770c94b5042f2270013
Kevin K <kbknapp@gmail.com>
2016-03-28 19:09:13 -0400

    fix: fixes mangatown downloader

--- src/mrcrawl.py
+++ src/mrcrawl.py
@@ -11,7 +11,9 @@ import zipfile
 # TODO:
 
 MANGAREADER = 'http://www.mangareader.net/{}/{}/{}'
-MANGATOWN = 'http://www.mangatown.com/manga/{}/c{:0>3}/{}.html'
+MANGATOWN = 'http://www.mangatown.com/manga/{}/{}c{:0>3}/{}.html'
+FILE_NAME = '{}_{:0>3}_{:0>3}.jpg'
+CBR = '{}_ch{:0>3}.cbr'
 
 def md5sum(filename):
     with open(filename, mode='rb') as f:
@@ -46,35 +48,60 @@ def parse_cli():
             long='--src',\
             args_taken=1, \
             help='Source to download from (all lowercase, nospaces; i.e mangareader [default])')
+    app.new_arg('vol', \
+            short='-V', \
+            long='--volume',\
+            args_taken=1, \
+            help='Start at volume number (implies --volumes)')
+    app.new_arg('vols', \
+            long='--volumes',\
+            help='This manga uses volumes in the URL (mangatown only)')
+    app.new_arg('verb', \
+            long='--verbose',\
+            help='Use verbose output')
 
     return app.start()
 
-def main(cxt, manga_url=MANGAREADER):
+def make_cbr(imgs, manga, chapter):
+    print('\n -> Creating CBR...', end='', flush=True)
+    zip = zipfile.ZipFile(CBR.format(manga, chapter), 'w')
+    for img in imgs:
+        zip.write(img)
+        os.remove(img)
+    zip.close()
+    print('Done')
+
+def cleanup(imgs):
+    print(' -> Cleaning Up...', end='', flush=True)
+    for img in imgs:
+       os.remove(img)
+    print('Done')
+
+def mangareader(cxt):
+    print('Downloading {} from {}'.format(cxt['manga'], 'mangareader'))
     # prefix for CBR and JPG files
     manga = cxt['manga']
-    file_name = '{}_{:0>3}_{:0>3}.jpg'
-    cbr = '{}_ch{:0>3}.cbr'
 
     chapter = 0
     if cxt['chapter']:
-        chapter = int(cxt['chapter']) - 1
+        chapter = int(cxt['chapter'][0]) - 1
     page = 0
     if cxt['page']:
-        chapter = int(cxt['page']) - 1
+        page = int(cxt['page'][0]) - 1
     curr = ''
     prev = ''
     dups = 0
 
     while True:
         chapter += 1
-        url = manga_url.format(manga, chapter, page)
+        url = MANGAREADER.format(manga, chapter, page)
         if not requests.get(url):
             break
         print(':: Downloading Chapter...{}'.format(chapter))
         imgs = []
         while True:
             page += 1
-            url = manga_url.format(manga, chapter, page)
+            url = MANGAREADER.format(manga, chapter, page)
             req = requests.get(url)
             if req:
                 html_txt = requests.get(url).text
@@ -83,7 +110,7 @@ def main(cxt, manga_url=MANGAREADER):
                 img = img_urls[0]
                 print(' -> Page...%d\r'%page, end='')
                 #print('{}'.format(page), end='', flush=True)
-                img_name = file_name.format(manga, str(chapter).rjust(3, '0'), str(page).rjust(3, '0'))
+                img_name = FILE_NAME.format(manga, str(chapter).rjust(3, '0'), str(page).rjust(3, '0'))
                 with open(img_name, 'wb') as f:
                     f.write(requests.get(img).content)
                 curr = md5sum(img_name)
@@ -91,11 +118,8 @@ def main(cxt, manga_url=MANGAREADER):
                     dups += 1
                     if dups > 3:
                         print('\n -> Found Multiple Duplicates')
-                        print(' -> Cleaning Up...', end='', flush=True)
                         imgs.append(img_name)
-                        for img in imgs:
-                           os.remove(img)
-                        print('Done')
+                        cleanup(imgs)
                         return 
                 else:
                     prev = curr
@@ -103,21 +127,114 @@ def main(cxt, manga_url=MANGAREADER):
                 imgs.append(img_name)
             else:
                 page = 0
-                print('\n -> Creating CBR...', end='', flush=True)
-                zip = zipfile.ZipFile(cbr.format(manga, chapter), 'w')
-                for img in imgs:
-                    zip.write(img)
-                    os.remove(img)
-                zip.close()
-                print('Done')
+                make_cbr(imgs, manga, chapter)
                 break
+
+def mangatown(cxt):
+    print(':: Downloading {} from {}'.format(cxt['manga'], 'mangatown'))
+    verb = cxt['verb']
+    # prefix for CBR and JPG files
+    manga = cxt['manga']
+
+    chapter = 1
+    if cxt['chapter']:
+        chapter = int(cxt['chapter'][0])
+    page = 0
+    if cxt['page']:
+        page = int(cxt['page'][0])
+    vol = 1
+    uses_vols = (cxt['vol'] or cxt['vols'])
+    if uses_vols:
+        if verb: print(':: Using volumes')
+        if cxt['vol']:
+            vol = int(cxt['vol']) 
+            if verb: print(':: Starting with volume...{}'.format(vol))
+    incd_ch   = False
+    incd_vol  = False
+
+    while True:
+        vol_str = ''
+        if cxt['vol'] or cxt['vols']:
+            vol_str = 'v{:0>2}/'.format(vol)
+        url = MANGATOWN.format(manga, vol_str, chapter, page)
+        if verb: print(':: Using URL...{}'.format(url))
+        print(':: Downloading Chapter...{}'.format(chapter))
+        imgs = []
+
+        while True:
+            url = MANGATOWN.format(manga, vol_str, chapter, page)
+            req = requests.get(url)
+            if req:
+                if verb: print(' -> Good Request')
+                html_txt = requests.get(url).text
+                doc = html.fromstring(html_txt)
+                img_urls = [i.attrib['src'] for i in doc.cssselect('img')]
+                img = img_urls[0]
+                link_urls = [i.attrib['href'] for i in doc.cssselect('a') if i.get('onclick') == 'return next_page();']
+                if verb: print(' -> Found next page URL...{}'.format(link_urls[0]))
+                if link_urls[0] == 'javascript:void(0);' and not incd_ch:
+                    if verb: 
+                        print(' -> No valid link for next page')
+                        print(' -> Incrementing chapter number')
+                    # if incd_vol:
+                    #     vol -= 1
+                    #     incd_vol = False
+                    chapter += 1
+                    page = 1
+                    incd_ch = True
+                    if imgs:
+                        make_cbr(imgs, manga, chapter)
+                        imgs = []
+                    break
+                elif link_urls[0] == 'javascript:void(0);' and uses_vols and not incd_vol:
+                    if verb: 
+                        print(' -> No valid link for next page')
+                        print(' -> Incrementing volume number')
+                    vol += 1
+                    # chapter -= 1
+                    page = 1
+                    incd_vol = True
+                    if imgs:
+                        make_cbr(imgs, manga, chapter)
+                        imgs = []
+                    break
+                elif link_urls[0] == 'javascript:void(0);' and incd_ch and (incd_vol or not uses_vols):
+                    if verb: 
+                        print(' -> No valid link for next page')
+                        print(' -> Done')
+                    if imgs:
+                        make_cbr(imgs, manga, chapter)
+                        imgs = []
+                    return
+                else:
+                    incd_vol = False
+                    incd_ch = False
+                url = link_urls[0]
+                if not verb:
+                    print(' -> Downloading Page...%d\r'%page, end='')
+                else:
+                    print(' -> Downloading Page...%d'%page)
+                img_name = FILE_NAME.format(manga, str(chapter).rjust(3, '0'), str(page).rjust(3, '0'))
+                with open(img_name, 'wb') as f:
+                    if verb: print(' -> Using IMG URL...{}'.format(img))
+                    f.write(requests.get(img).content)
+                imgs.append(img_name)
+            else:
+                if verb: 
+                    print(' -> Bad Request')
+                    print(' -> Resetting Page Number')
+                    print(' -> Incrementing Chapter Number')
+                page = 1
+                make_cbr(imgs, manga, chapter)
+                chapter += 1
+                break
+            page += 1
 
 if __name__ == '__main__':
     cxt = parse_cli()
+    src = ''
     if cxt['src']:
         src = cxt['src'][0]
-        if src == 'mangatown':
-            print(':: Downloading {} from {}'.format(cxt['manga'], 'mangatown'))
-            sys.exit(main(cxt, MANGATOWN))
-    print('Downloading {} from {}'.format(cxt['manga'], 'mangareader'))
-    sys.exit(main(cxt))
+    if src == 'mangatown' or cxt['vol'] or cxt['vols']:
+        sys.exit(mangatown(cxt))
+    sys.exit(mangareader(cxt))
